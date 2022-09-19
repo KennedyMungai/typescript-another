042f9cb7830ada1d31136377c8df1d5dd32e3585
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-19 15:55:50 +0300

    Did some code refactoring

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,7 +3,15 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url)
-    .then(response => {
-        console.log(response.data);
+axios.get(url).then(response => {
+        const todo = response.data;
+        const ID = todo.id;
+        const title = todo.title;
+        const finished =  todo.completed;
+
+        console.log(`
+            The TODO with ID: ${ID}
+            Has a title of ${title}
+            Is it finished?: ${finished}
+        `);
     });
