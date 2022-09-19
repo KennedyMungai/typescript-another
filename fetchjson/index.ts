2b890ace7bdd2f17723411858c9219417d184f49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-19 15:49:50 +0300

    Logged out the json data that was received

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,4 +3,7 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url);
+axios.get(url)
+    .then(response => {
+        console.log(response.data);
+    });
