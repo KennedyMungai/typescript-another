e20a1a0ca77479beeabfb82a0c331ddf5a22a73c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 15:35:55 +0300

    Passed in extra options for the map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -5,4 +5,10 @@ import { User } from "./User";
 const user = new User();
 const company = new Company();
 
-new google.maps.Map(document.getElementById('map'));
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
