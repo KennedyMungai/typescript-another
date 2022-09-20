a9f5dd5cbfd93cabe78c6666210ffde6b5aade31
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 14:30:15 +0300

    Added fields inside the class Company

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -2,5 +2,10 @@ import faker from 'faker';
 
 
 export class Company {
-    
+    companyName: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
 }
