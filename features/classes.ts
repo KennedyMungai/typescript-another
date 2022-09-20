3fb7c125196eabd3887cd15420feb6c5e14938cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 12:31:24 +0300

    Created and called the honk function

--- features/classes.ts
+++ features/classes.ts
@@ -2,7 +2,12 @@ class Vehicle {
     drive(): void {
         console.log("Something something fast in mu car");
     }
+
+    honk(): void {
+        console.log("Honk honk");
+    }
 }
 
 const vehicle = new Vehicle();
 vehicle.drive();
+vehicle.honk();
