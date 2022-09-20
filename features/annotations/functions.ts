a37add784fa8ceed1eb14247abb7c550f75d797f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 10:26:38 +0300

    Created the throwError function which returns a variable of type never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -9,3 +9,7 @@ function divide(a: number, b: number): number {
 const logger = (message: string): void => {
     console.log(message);
 };
+
+const throwError = ( message: string ): never => {
+    throw new Error(message);
+};
