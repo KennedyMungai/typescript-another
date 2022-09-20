a3bf018728a910930a50e9792e2531c50fd6278e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 11:59:29 +0300

    Created the skeleton for the printVehicle function

---
+++ features/interfaces.ts
@@ -0,0 +1,9 @@
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true
+};
+
+const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+    
+};      
