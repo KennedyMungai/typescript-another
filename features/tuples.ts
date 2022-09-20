5777a7974bac5273c3d35c58609adebf7b98b5ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-20 11:51:45 +0300

    Used better notation for the tuple

--- features/tuples.ts
+++ features/tuples.ts
@@ -4,4 +4,6 @@ const drink = {
     sugar: 40,
 };
 
-const pepsi: [string, boolean, number] = ['brown', true, 40];
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
