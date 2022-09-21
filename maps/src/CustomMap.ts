47f90fde138cf4ead17576e3620e4703213105da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-21 10:57:30 +0300

    Updated the document selector statement

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -9,7 +9,7 @@ export class CustomMap {
     private googleMap: google.maps.Map;
 
     constructor(divId: string) {
-        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+        this.googleMap = new google.maps.Map(document.getElementById('map'), {
             zoom: 1,
             center: {
                 lat: 0,
